33537c2065f40fdc663a525e1174bcd8f0446a21
Mohammed Lajam <88610375+MohammedLajam@users.noreply.github.com>
2022-07-20 10:17:31 +0200

    update 9*9 grid

--- Sudoku-Solver-V2.py
+++ Sudoku-Solver-V2.py
@@ -8,17 +8,17 @@ def solver(sudoku, solutions):
     uniquely the numbers from 1 to 6, in such a way that on every row and column there are no repeated numbers.
     """
     # checking if the cell is empty (searching for ZEROS):
-    for row in range(0, 6):
-        for column in range(0, 6):
+    for row in range(0, 9):
+        for column in range(0, 9):
             if sudoku[row][column] == 0:
-                for number in range(1, 7):
+                for number in range(1, 10):
                     # applying sudoku conditions
                     # checking if the iterated number in a specific row, column or square (2*3)
-                    x = row // 2  # 0, 0, 1, 1, 2, 2 for every row iteration
-                    y = column // 3  # 0, 0, 0, 1, 1, 1 for every column iteration
+                    x = row // 3  # 0, 0, 0, 1, 1, 1, 2, 2, 2 for every row iteration
+                    y = column // 3  # 0, 0, 0, 1, 1, 1, 2, 2, 2 for every column iteration
                     if number not in sudoku[row, :] \
                         and number not in sudoku[:, column] \
-                            and number not in sudoku[x * 2:(x + 1) * 2, y * 3:(y + 1) * 3]:
+                            and number not in sudoku[x * 3:(x + 1) * 3, y * 3:(y + 1) * 3]:
                         sudoku[row][column] = number
                         solver(sudoku, solutions)
                         sudoku[row][column] = 0
@@ -30,19 +30,22 @@ def solver(sudoku, solutions):
 # The Sudoku puzzle to be tested:
 if __name__ == "__main__":
     s = np.array(
-            [0, 0, 3, 0, 0, 0,
-             5, 6, 0, 3, 2, 0,
-             0, 5, 4, 2, 0, 0,
-             2, 0, 6, 0, 5, 0,
-             0, 1, 2, 0, 4, 0,
-             0, 0, 0, 1, 0, 0]
-        ).reshape([6, 6])
+        [7, 8, 0, 4, 0, 0, 1, 2, 0,
+         6, 0, 0, 0, 7, 5, 0, 0, 9,
+         0, 0, 0, 6, 0, 1, 0, 7, 8,
+         0, 0, 7, 0, 4, 0, 2, 6, 0,
+         0, 0, 1, 0, 5, 0, 9, 3, 0,
+         9, 0, 4, 0, 6, 0, 0, 0, 5,
+         0, 7, 0, 3, 0, 0, 0, 1, 2,
+         1, 2, 0, 0, 0, 7, 4, 0, 0,
+         0, 4, 9, 2, 0, 6, 0, 0, 7]).reshape([9, 9])
 
     solutions_list = list()
     solver(s, solutions_list)
     for solution in solutions_list:
         print(solution)
 
+'''
     assert len(solutions_list) == 3, "Make sure there three are solutions to the problem"
     one_solution = np.array(
             [4, 2, 3, 5, 1, 6,
@@ -53,3 +56,4 @@ if __name__ == "__main__":
              6, 4, 5, 1, 3, 2]
         ).reshape([6, 6])
     assert any(np.array_equal(sol, one_solution) for sol in solutions_list), "Make sure one of the solutions match"
+'''
